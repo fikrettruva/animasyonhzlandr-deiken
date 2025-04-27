43af7b46aae1b1aab3568609449f70823b2ad85a
fikrettruva <fikret20091@hotmail.com>
2025-04-27 20:41:18 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+input.onButtonPressed(Button.A, function () {
+    zaman += 100
+})
+input.onButtonPressed(Button.B, function () {
+    zaman += -100
+})
+let zaman = 300
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(zaman)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.pause(zaman)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(zaman)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(zaman)
+})
